33fbb3ea30f5e4395c9b3a5ecba868edeb239ac5
Jan Aagaard <jan@aagaard.net>
2016-02-29 23:32:26 +0100

    Crawling all current movies and fixed a bug in movies without an original title

--- crawler/kino/spiders/movies_spider.py
+++ crawler/kino/spiders/movies_spider.py
@@ -5,7 +5,15 @@ from kino.items import MovieItem
 class MoviesSpider(scrapy.Spider):
     name = "movies"
     allowed_domains = ["kino.dk"]
-    start_urls = ["http://www.kino.dk/aktuelle-film?page=0"]
+    start_urls = [
+        "http://www.kino.dk/aktuelle-film?page=0",
+        "http://www.kino.dk/aktuelle-film?page=1",
+        "http://www.kino.dk/aktuelle-film?page=2",
+        "http://www.kino.dk/aktuelle-film?page=3",
+        "http://www.kino.dk/aktuelle-film?page=4",
+        "http://www.kino.dk/aktuelle-film?page=5",
+        "http://www.kino.dk/aktuelle-film?page=6"
+    ]
 
     def parse(self, response):
         for movie_href in response.css('.movies-list-inner-wrap').xpath('./h2/a/@href'):
@@ -16,6 +24,8 @@ class MoviesSpider(scrapy.Spider):
         movie = MovieItem()
         movie['danish_title'] = response.css('.node-title').xpath('text()').extract()[0].strip()
         movie['movie_url'] = response.url
-        movie['original_title'] = response.css('.field-field-movie-original-title .field-item').xpath('text()[2]').extract()[0].strip()
+        original_title_field = response.css('.field-field-movie-original-title .field-item')
+        if len(original_title_field) > 0:
+            movie['original_title'] = original_title_field.xpath('text()[2]').extract()[0].strip()
         movie['poster_url'] = response.css('.field-field-movie-poster-image .field-item').xpath('img/@src').extract()[0]
         yield movie
