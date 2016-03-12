e094db9fc782cf8c8e98cbe501377461072432c4
Jan Aagaard <jan@aagaard.net>
2016-03-12 21:38:16 +0100

    Fetching showings with callback to same URL

--- crawler/kino/spiders/showings_spider.py
+++ crawler/kino/spiders/showings_spider.py
@@ -24,30 +24,62 @@ class TheatersSpider(scrapy.Spider):
                 version_item = title_item.xpath('h3/a/span/text()')
                 if len(version_item) >= 1:
                     # Strip the first dash.
-                    version = version_item[0].re(r' - (.*)')
+                    version = version_item.re(r' - (.*)')[0]
                 else:
                     # No version info - defaulting to just calling this a 2D movie.
                     version = '2D'
 
-                value = title_item.xpath('@value').extract_first()
-                showings_table = title_item.xpath('../div[@class="cinema-movie clearfix"]/div[@value="' + value + '"]')
-                for showings_column in showings_table.css('.cinema-movie-dates').xpath('li'):
-                    for showing_cell in showings_column.xpath('ul/li/a'):
-                        dayAndMonth = showings_column.xpath('div[2]/text()').extract_first().split('/')
-                        day = int(dayAndMonth[0])
-                        month = int(dayAndMonth[1])
-                        hourAndMinute = showing_cell.xpath('text()').extract_first().split(':')
-                        hour = int(hourAndMinute[0])
-                        minute = int(hourAndMinute[1])
-                        date_obj = datetime(2016, month, day, hour, minute)
+                request = scrapy.Request(response.url, callback=self.parse_showings_table)
+                request.meta['movie_url'] = movie_url
+                request.meta['showings_table_value'] = title_item.xpath('@value').extract_first()
+                request.meta['theater_url'] = response.url
+                request.meta['version'] = version
+                yield request
 
-                        showing = ShowingItem()
-                        showing['movie_url'] = movie_url
-                        showing['theater_url'] = response.url
-                        showing['showing_url'] = response.urljoin(showing_cell.xpath('@href').extract_first())
-                        showing['start'] = date_obj.strftime('%Y-%m-%d %H:%M:00')
-                        showing['version'] = version
-                        yield showing
+                #showings_table = title_item.xpath('../div[@class="cinema-movie clearfix"]/div[@value="' + value + '"]')
+                # for showings_column in showings_table.css('.cinema-movie-dates').xpath('li'):
+                #     for showing_cell in showings_column.xpath('ul/li/a'):
+                #         dayAndMonth = showings_column.xpath('div[2]/text()').extract_first().split('/')
+                #         day = int(dayAndMonth[0])
+                #         month = int(dayAndMonth[1])
+                #         hourAndMinute = showing_cell.xpath('text()').extract_first().split(':')
+                #         hour = int(hourAndMinute[0])
+                #         minute = int(hourAndMinute[1])
+                #         date_obj = datetime(2016, month, day, hour, minute)
+
+                #         showing = ShowingItem()
+                #         showing['movie_url'] = movie_url
+                #         showing['theater_url'] = response.url
+                #         showing['showing_url'] = response.urljoin(showing_cell.xpath('@href').extract_first())
+                #         showing['start'] = date_obj.strftime('%Y-%m-%d %H:%M:00')
+                #         showing['version'] = version
+                #         yield showing
+
+    def parse_showings_table(self, response):
+        movie_url = response.meta['movie_url']
+        showings_table_value = response.meta['showings_table_value']
+        theater_url = response.meta['theater_url']
+        version = response.meta['version']
+
+        showings_table = response.xpath('//div[@class="cinema-movie clearfix"]/div[@value="' + showings_table_value + '"]')
+        for showings_column in showings_table.css('.cinema-movie-dates').xpath('li'):
+            for showing_cell in showings_column.xpath('ul/li/a'):
+                dayAndMonth = showings_column.xpath('div[2]/text()').extract_first().split('/')
+                day = int(dayAndMonth[0])
+                month = int(dayAndMonth[1])
+                hourAndMinute = showing_cell.xpath('text()').extract_first().split(':')
+                hour = int(hourAndMinute[0])
+                minute = int(hourAndMinute[1])
+                date_obj = datetime(2016, month, day, hour, minute)
+
+                showing = ShowingItem()
+                showing['movie_url'] = movie_url
+                showing['theater_url'] = theater_url
+                showing['showing_url'] = response.urljoin(showing_cell.xpath('@href').extract_first())
+                showing['start'] = date_obj.strftime('%Y-%m-%d %H:%M:00')
+                showing['version'] = version
+                yield showing
+
 
 #                     showing['movie_url'] = response.meta['movie_url']
 #                     showing['theater_url'] = response.urljoin(movie_div.xpath('h3/a/@href')[0].extract())
