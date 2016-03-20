f92fa4a1b0ea5fa702aaa5ddec9004eb5b23bb3f
Jan Aagaard <jan@aagaard.net>
2016-03-20 13:55:14 +0100

    TODOs added about removing the hash part of the URL

--- crawler/kino/spiders/showings_spider.py
+++ crawler/kino/spiders/showings_spider.py
@@ -50,6 +50,7 @@ class TheatersSpider(scrapy.Spider):
         if len(jump_links) >= 1:
             jump_link = jump_links[-1]
             if jump_link.xpath('text()').extract_first().endswith(u'>'):
+                # TODO: Might want to remove the hash part of the URL since it seems that it's being cached separately.
                 jump_url = response.urljoin(jump_link.xpath('@href').extract_first())
                 request = scrapy.Request(jump_url, callback=self.parse_showings_table)
                 request.meta['movie_url'] = movie_url
@@ -81,6 +82,7 @@ class TheatersSpider(scrapy.Spider):
             if at_least_one_showing_found:
                 next_page = showings_table.css('.showtimes-extra').xpath('a[last()]')
                 if next_page:
+                    # TODO: Might want to remove the hash part of the URL since it seems that it's being cached separately.
                     next_page_url = response.urljoin(next_page.xpath('@href')[0].extract())
                     request = scrapy.Request(next_page_url, callback=self.parse_showings_table)
                     request.meta['movie_url'] = movie_url
