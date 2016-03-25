44c3e459e9c9c7f82a2fab65d16bdff424d9080d
Jan Aagaard <jan@aagaard.net>
2016-03-25 15:25:46 +0100

    Running all three crawls with as the default gulp command

--- crawler/gulpfile.js
+++ crawler/gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require("gulp")
+var runSequence = require('run-sequence');
 var shell = require("gulp-shell")
 
 // Crawling of the same URL has been allowed in settings.py to make showings crawling work. But this results in some duplicates of the movies occurring. So they are removed manually after the crawl.
@@ -10,11 +11,18 @@ gulp.task("crawl-movies", shell.task([
     "mv output/movies-without-duplicates.json output/movies.json"
 ]))
 
+gulp.task("crawl-showings", shell.task([
+    "rm output/showings.json",
+    "scrapy crawl showings"
+]))
+
 gulp.task("crawl-theaters", shell.task([
     "rm output/theaters.json",
     "scrapy crawl theaters"
 ]))
 
-gulp.task("default", function() {
-  // place code for your default task here
-})
+gulp.task("default", runSequence(
+    "crawl-movies",
+    "crawl-showings",
+    "crawl-theaters"
+))
