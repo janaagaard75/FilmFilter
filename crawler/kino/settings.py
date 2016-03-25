9afac8f35b361b07023f9b32b318693000f8838d
Jan Aagaard <jan@aagaard.net>
2016-03-25 14:25:39 +0100

    Cleanup in Scrapy settngs file

--- crawler/kino/settings.py
+++ crawler/kino/settings.py
@@ -1,19 +1,9 @@
 # -*- coding: utf-8 -*-
 
-# Scrapy settings for kino project
-#
-# For simplicity, this file contains only settings considered important or
-# commonly used. You can find more settings consulting the documentation:
-#
-#     http://doc.scrapy.org/en/latest/topics/settings.html
-#     http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
-#     http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
-
 BOT_NAME = 'kino'
 
 SPIDER_MODULES = ['kino.spiders']
 NEWSPIDER_MODULE = 'kino.spiders'
-
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 USER_AGENT = 'FilmFinder'
@@ -21,49 +11,8 @@ USER_AGENT = 'FilmFinder'
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS=32
 
-# Configure a delay for requests for the same website (default: 0)
-# See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
-# See also autothrottle settings and docs
-#DOWNLOAD_DELAY=3
-# The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN=16
-#CONCURRENT_REQUESTS_PER_IP=16
-
 # Disable cookies (enabled by default)
 COOKIES_ENABLED=False
-
-# Disable Telnet Console (enabled by default)
-#TELNETCONSOLE_ENABLED=False
-
-# Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
-#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-#   'Accept-Language': 'en',
-#}
-
-# Enable or disable spider middlewares
-# See http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
-#    'kino.middlewares.MyCustomSpiderMiddleware': 543,
-#}
-
-# Enable or disable downloader middlewares
-# See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'kino.middlewares.MyCustomDownloaderMiddleware': 543,
-#}
-
-# Enable or disable extensions
-# See http://scrapy.readthedocs.org/en/latest/topics/extensions.html
-#EXTENSIONS = {
-#    'scrapy.telnet.TelnetConsole': None,
-#}
-
-# Configure item pipelines
-# See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
-# ITEM_PIPELINES = {
-#    'kino.pipelines.KinoPipeline': 500,
-# }
 
 # Using a feed exporter instead of the pipeline.
 FEED_EXPORTERS = {
@@ -77,12 +26,6 @@ FEED_FORMAT='json'
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
 # NOTE: AutoThrottle will honour the standard settings for concurrency and delay
 AUTOTHROTTLE_ENABLED=True
-# The initial download delay
-#AUTOTHROTTLE_START_DELAY=5
-# The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY=60
-# Enable showing throttling stats for every response received:
-#AUTOTHROTTLE_DEBUG=False
 
 # Enable and configure HTTP caching (disabled by default)
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
@@ -90,7 +33,8 @@ HTTPCACHE_ENABLED=True
 HTTPCACHE_EXPIRATION_SECS=3600 # 1 hour. Pages aren't cached between crawls.
 HTTPCACHE_STORAGE='scrapy.extensions.httpcache.FilesystemCacheStorage'
 
+# Turn off de-duplication of URls since we visit the same URLs multiple times when crawling showings.
 DUPEFILTER_CLASS = 'kino.filters.NoDupeFilter'
 
-#  'CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG'
+# Possilbe values: 'CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG'.
 LOG_LEVEL='INFO'
