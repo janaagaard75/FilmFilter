b2caa954d93d7e50319fed5ef0029124bdaaccdb
Jan Aagaard <jan@aagaard.net>
2016-03-25 14:20:12 +0100

    Split shell command into two

--- crawler/gulpfile.js
+++ crawler/gulpfile.js
@@ -1,9 +1,10 @@
 var gulp = require("gulp")
 var shell = require("gulp-shell")
 
-gulp.task("crawl-theaters", shell.task(
-    "rm output/theaters.json & scrapy crawl theaters"
-))
+gulp.task("crawl-theaters", shell.task([
+    "rm output/theaters.json",
+    "scrapy crawl theaters"
+]))
 
 gulp.task("default", function() {
   // place code for your default task here
