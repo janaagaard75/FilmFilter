ae1702f7d745b6ac0a8fcf40f8b839d4373b6a1b
Jan Aagaard <jan@aagaard.net>
2016-03-25 14:26:35 +0100

    Gulp command for crawling movies

--- crawler/gulpfile.js
+++ crawler/gulpfile.js
@@ -6,6 +6,14 @@ gulp.task("crawl-theaters", shell.task([
     "scrapy crawl theaters"
 ]))
 
+gulp.task("crawl-movies", shell.task([
+    "rm output/movies.json",
+    "scrapy crawl movies",
+    "sort output/movies.json | uniq -u > output/movies-without-duplicates.json",
+    "rm output/movies.json",
+    "mv output/movies-without-duplicates.json output/movies.json"
+]))
+
 gulp.task("default", function() {
   // place code for your default task here
 })
