c757523f1778ec2c7c4c819c24b3b1903ebdc8fb
Jan Aagaard <jan@aagaard.net>
2016-04-02 20:20:45 +0200

    Using Gulp to copy files to dist folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,34 @@
 "use strict";
 
-const fs = require('fs');
 const gulp = require("gulp")
-const moment = require('moment')
-const runSequence = require('run-sequence')
-const shell = require("gulp-shell")
 
-function deleteIfExists (path, done) {
-    fs.access(path, fs.R_OK | fs.W_OK, (error) => {
-        if (!error) {
-            fs.unlinkSync(path)
-        }
+function deleteIfExists(path, done) {
+  fs.access(path, fs.R_OK | fs.W_OK, (error) => {
+    if (!error) {
+      fs.unlinkSync(path)
+    }
 
-        done()
-    })
+    done()
+  })
 }
 
-gulp.task("build", )
-
-gulp.task("default", done => {
-    runSequence(
-        "movies",
-        "showings",
-        "theaters",
-        done)
+gulp.task("copy-html-files", () => {
+  return gulp
+    .src("src/*.html")
+    .pipe(gulp.dest("dist"))
 })
+
+gulp.task("copy-javascript-files", () => {
+  return gulp
+    .src("src/*.js")
+    .pipe(gulp.dest("dist"))
+})
+
+gulp.task("build", [
+  "copy-html-files",
+  "copy-javascript-files"]
+)
+
+gulp.task("default", [
+  "build"
+])
