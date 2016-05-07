4b74d6fc2ac72fa5f8ab811497e66b6a3002a2b4
Jan Aagaard <jan@aagaard.net>
2016-05-07 17:30:37 +0200

    Get started on main component

---
+++ website/src/FilmFinderApp.tsx
@@ -0,0 +1,10 @@
+import * as React from "react"
+
+export interface IAppProps {
+}
+
+export interface IAppState {
+}
+
+export default class FilmFinderApp extends React.Component<IAppProps, IAppState> {
+}
