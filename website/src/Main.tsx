f96a4f904e1bc971a434173ba3e5df59e488737e
Jan Aagaard <jag@zmags.com>
2016-05-08 17:25:15 +0200

    Remove unnecesary semicolon

--- website/src/Main.tsx
+++ website/src/Main.tsx
@@ -1,6 +1,6 @@
 // TODO: Try using react-global instead. See https://github.com/palantir/tslint/issues/589.
 /* tslint:disable:no-unused-variable */
-import * as React from "react";
+import * as React from "react"
 /* tslint:enable:no-unused-variable */
 import * as ReactDOM from "react-dom"
 import FilmFilterApp from "./FilmFilterApp"
