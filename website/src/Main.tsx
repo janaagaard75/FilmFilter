9c9619e0374c61d6984f2eb756fbefd6b1b99e43
Jan Aagaard <jag@zmags.com>
2016-05-09 08:20:34 +0200

    Load data from scraper output

--- website/src/Main.tsx
+++ website/src/Main.tsx
@@ -4,9 +4,17 @@ import * as React from "react"
 /* tslint:enable:no-unused-variable */
 import * as ReactDOM from "react-dom"
 import FilmFilterApp from "./FilmFilterApp"
-import { IShowing } from "./Showing"
 
-const showings: Array<IShowing> = require("./showings-demo-data.json")
+export interface ShowingData {
+  "start": string
+  "showingUrl": string
+  "version": string
+  "theaterUrl": string
+  "movieUrl": string
+}
+
+// TODO: Load the data using an AJAX call instead of embedding it the bundle.
+const showings: Array<ShowingData> = require("../../scraper/output/showings-20160508212716.json")
 
 ReactDOM.render(
   <FilmFilterApp showings={showings}/>,
