6cfe4e72230bcc21ff79a5c4fde425886005dfc2
Jan Aagaard <jag@zmags.com>
2016-05-09 09:01:43 +0200

    Note about React Refetch

--- website/src/Main.tsx
+++ website/src/Main.tsx
@@ -13,7 +13,7 @@ export interface ShowingData {
   "movieUrl": string
 }
 
-// TODO: Load the data using an AJAX call instead of embedding it the bundle.
+// TODO: Load the data using an AJAX call instead of embedding it the bundle. React Refetch might be the rigth tool: https://engineering.heroku.com/blogs/2015-12-16-react-refetch/
 const showings: Array<ShowingData> = require("../../scraper/output/showings.json")
 
 ReactDOM.render(
