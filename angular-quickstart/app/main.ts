bc0633340a5d1c3f0592c64e10eeef6666f80a5e
Jan Aagaard <jan@aagaard.net>
2016-10-23 21:26:05 +0200

    Add reference to next step

--- angular-quickstart/app/main.ts
+++ angular-quickstart/app/main.ts
@@ -4,3 +4,5 @@ import { AppModule } from './app.module';
 
 const platform = platformBrowserDynamic();
 platform.bootstrapModule(AppModule);
+
+// TODO: Next step: https://angular.io/docs/ts/latest/tutorial/toh-pt6.html
