00455e8495c9d9399f936e1a9622857701fe369d
Jan Aagaard <jan@aagaard.net>
2016-10-23 21:06:20 +0200

    Add dashboard

--- angular-quickstart/app/app.module.ts
+++ angular-quickstart/app/app.module.ts
@@ -1,20 +1,51 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { HeroDetailComponent } from './hero-detail.component'
+import { DashboardComponent } from './dashboard.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
+
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule
+  bootstrap: [
+    AppComponent
   ],
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent
   ],
-  bootstrap: [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: 'detail/:id',
+        component: HeroDetailComponent
+      },
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      }
+    ])
+  ],
+  providers: [
+    HeroService
+  ]
 })
-
-export class AppModule { }
+export class AppModule {
+}
