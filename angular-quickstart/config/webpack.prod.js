c9b82be9e95a4231955f44fee1877a9fdddad0dc
Jan Aagaard <jan@aagaard.net>
2016-10-28 16:43:33 +0200

    Disable Dedupe plugin

--- angular-quickstart/config/webpack.prod.js
+++ angular-quickstart/config/webpack.prod.js
@@ -23,7 +23,8 @@ module.exports = webpackMerge(commonConfig, {
 
   plugins: [
     new webpack.NoErrorsPlugin(),
-    new webpack.optimize.DedupePlugin(),
+    // TODO: Waiting for https://github.com/webpack/webpack/issues/2644 to be solved.
+    // new webpack.optimize.DedupePlugin(),
     new webpack.optimize.UglifyJsPlugin({ // https://github.com/angular/angular/issues/10618
       mangle: {
         keep_fnames: true
