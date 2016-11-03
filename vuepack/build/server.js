dfcc102940642dd020d7680899f7c92b94c883a3
Jan Aagaard <jan@aagaard.net>
2016-11-03 22:09:09 +0100

    Copy to vuepack

---
+++ vuepack/build/server.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const app = express()
+const config = require('./webpack.dev')
+const compiler = webpack(config)
+const express = require('express')
+const path = require('path')
+const webpack = require('webpack')
+
+const devMiddleWare = require('webpack-dev-middleware')(compiler,
+  {
+    publicPath: config.output.publicPath,
+    stats: {
+      colors: true,
+      modules: false,
+      children: false,
+      chunks: false,
+      chunkModules: false
+    }
+  })
+
+app.use(devMiddleWare)
+app.use(require('webpack-hot-middleware')(compiler))
+
+app.get('*', (req, res) => {
+  const fs = devMiddleWare.fileSystem
+
+  devMiddleWare.waitUntilValid(() => {
+    res.end(fs.readFileSync(path.join(config.output.path, '../index.html')))
+  })
+})
+
+app.listen(8080, () => {
+  console.log('Site available on http://localhost:8080/')
+})
---
+++ vuepack/client/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="page">
+    <counter></counter>
+    <p>To get started, edit the file in <code>./client</code> and save it.</p>
+  </div>
+</template>
+
+<script>
+  import Counter from '../components/Counter'
+
+  export default {
+    components: {
+      Counter
+    }
+  }
+</script>
