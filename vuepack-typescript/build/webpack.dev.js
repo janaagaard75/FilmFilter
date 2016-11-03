c3153b3b5876252aa087b703978acfb7166e685d
Jan Aagaard <jan@aagaard.net>
2016-11-03 19:58:04 +0100

    Use webpack-merge

--- vuepack-typescript/build/webpack.dev.js
+++ vuepack-typescript/build/webpack.dev.js
@@ -2,35 +2,36 @@
 const webpack = require('webpack')
 const base = require('./webpack.base')
 const _ = require('./utils')
+const webpackMerge = require('webpack-merge')
 
-base.devtool = 'eval-source-map'
-base.plugins.push(
-  new webpack.DefinePlugin({
-    'process.env.NODE_ENV': JSON.stringify('development')
-  }),
-  new webpack.HotModuleReplacementPlugin(),
-  new webpack.NoErrorsPlugin()
-)
-
-// Push loader for .css file.
-base.module.rules.push(
-  {
-    test: /\.css$/,
-    loader: [
+module.exports = webpackMerge.smart(base, {
+  devtool: 'eval-source-map',
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development')
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  module: {
+    rules: [
       {
-        loader: 'style-loader'
-      },
-      {
-        loader: 'css-loader',
-        options: {
-          autoprefixer: false
-        }
-      },
-      {
-        loader:'postcss-loader'
+        test: /\.css$/,
+        loader: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              autoprefixer: false
+            }
+          },
+          {
+            loader:'postcss-loader'
+          }
+        ]
       }
     ]
   }
-)
-
-module.exports = base
+})
