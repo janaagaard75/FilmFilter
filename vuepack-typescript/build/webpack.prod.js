c1504f4b403f3eedb3f0d417a5fdb759d6513e38
Jan Aagaard <jan@aagaard.net>
2016-11-03 18:55:32 +0100

    Use webpack-merge

--- vuepack-typescript/build/webpack.prod.js
+++ vuepack-typescript/build/webpack.prod.js
@@ -3,76 +3,91 @@ const exec = require('child_process').execSync
 const webpack = require('webpack')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const ProgressBarPlugin = require('progress-bar-webpack-plugin')
-const base = require('./webpack.base')
+const baseConfig = require('./webpack.base')
 const pkg = require('../package')
 const _ = require('./utils')
+const webpackMerge = require('webpack-merge')
 
+// TODO: Consider using rimraf instead.
 // Remove dist folder in web app mode.
 exec('rm -rf dist/')
-// Use source-map in web app mode.
-base.devtool = 'source-map'
 
-// TODO: Use webpack-merge instead of this base syntax. See https://github.com/AngularClass/angular2-webpack-starter/blob/master/config/webpack.prod.js
-
-// A white list to add dependencies to vendor chunk.
-base.entry.vendor = Object.keys(pkg.dependencies)
-// Use hash filename to support long-term caching.
-base.output.filename = '[name].[chunkhash:8].js'
-// Add webpack plugins.
-base.plugins.push(
-  new ProgressBarPlugin(),
-  new ExtractTextPlugin('styles.[contenthash:8].css'),
-  new webpack.DefinePlugin({
-    'process.env.NODE_ENV': JSON.stringify('production')
-  }),
-  new webpack.LoaderOptionsPlugin({
-    minimize: true
-  }),
-  // TODO: Uglyfy doesn't support ES6. See https://github.com/mishoo/UglifyJS2/issues/448.
-  // new webpack.optimize.UglifyJsPlugin({
-  //   sourceMap: true,
-  //   compress: {
-  //     warnings: false
-  //   },
-  //   output: {
-  //     comments: false
-  //   }
-  // }),
-  // Extract vendor chunks.
-  new webpack.optimize.CommonsChunkPlugin({
-    name: 'vendor',
-    filename: 'vendor.[chunkhash:8].js'
-  })
-)
-
-// Extrac css in standalone .css files.
-base.module.rules.push({
-  test: /\.css$/,
-  loader: ExtractTextPlugin.extract({
-    fallbackLoader: 'style-loader',
-    // TODO: Loaders is repeated in both dev and prod.
-    loader: [
+module.exports = webpackMerge.smart(baseConfig, {
+  // Use source-map in web app mode.
+  devtool: 'source-map',
+  entry: {
+    // A white list to add dependencies to vendor chunk.
+    vendor: Object.keys(pkg.dependencies)
+  },
+  output: {
+    // Use hash filename to support long-term caching.
+    filename: '[name].[chunkhash:8].js'
+  },
+  plugins: [
+    new ProgressBarPlugin(),
+    new ExtractTextPlugin('styles.[contenthash:8].css'),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    }),
+    // TODO: Uglyfy doesn't support ES6. See https://github.com/mishoo/UglifyJS2/issues/448.
+    // new webpack.optimize.UglifyJsPlugin({
+    //   sourceMap: true,
+    //   compress: {
+    //     warnings: false
+    //   },
+    //   output: {
+    //     comments: false
+    //   }
+    // }),
+    // Extract vendor chunks.
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      filename: 'vendor.[chunkhash:8].js'
+    })
+  ],
+  module: {
+    rules: [
       {
-        loader: 'style-loader'
+        // Extrac css in standalone .css files.
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          fallbackLoader: 'style-loader',
+          // TODO: Loaders is repeated in both dev and prod.
+          loader: [
+            {
+              loader: 'style-loader'
+            },
+            {
+              loader: 'css-loader',
+              options: {
+                autoprefixer: false
+              }
+            },
+            {
+              loader:'postcss-loader'
+            }
+          ]
+        })
       },
       {
-        loader: 'css-loader',
+        test: /\.vue$/,
         options: {
-          autoprefixer: false
+          loaders: ExtractTextPlugin.extract({
+            fallbackLoader: 'vue-style-loader',
+            loader: [
+              {
+                loader: 'css-loader',
+                options: {
+                  'autoprefixer': false
+                }
+              }
+            ]
+          })
         }
-      },
-      {
-        loader:'postcss-loader'
       }
     ]
-  })
+  }
 })
-
-// TODO: Figure out how to write this. The issue is adding this to the correct loader with the new Webpack 2 syntax.
-// extract css in single-file components
-// base.vue.loaders.css = ExtractTextPlugin.extract({
-//   loader: 'css-loader?-autoprefixer',
-//   fallbackLoader: 'vue-style-loader'
-// })
-
-module.exports = base
