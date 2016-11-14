4f041ada0efe120b6e674951fa89bc83bcaf261f
Jan Aagaard <jan@aagaard.net>
2016-11-14 16:03:45 +0100

    Simplify syntax

--- evaluation/todomvc-redux-react-typescript/client/todos/actions.ts
+++ evaluation/todomvc-redux-react-typescript/client/todos/actions.ts
@@ -4,32 +4,18 @@ import { assign } from 'lodash'
 import { TodoId, Todo } from './model'
 
 export const ADD_TODO = 'ADD_TODO'
+export const CLEAR_COMPLETED = 'CLEAR_COMPLETED'
+export const COMPLETE_ALL = 'COMPLETE_ALL'
+export const COMPLETE_TODO = 'COMPLETE_TODO'
 export const DELETE_TODO = 'DELETE_TODO'
 export const EDIT_TODO = 'EDIT_TODO'
-export const COMPLETE_TODO = 'COMPLETE_TODO'
-export const COMPLETE_ALL = 'COMPLETE_ALL'
-export const CLEAR_COMPLETED = 'CLEAR_COMPLETED'
 
 const actionCreator = actionCreatorFactory()
 
 // TODO: These action creators should be named differently. E.g. createAddTodo or todoCreated.
-const addTodo = actionCreator<string>(ADD_TODO)
-
-const deleteTodo = actionCreator<TodoId>(DELETE_TODO)
-
-const editTodo = actionCreator<{todoId: TodoId, newText: string}>(EDIT_TODO)
-
-const toggleTodo = actionCreator<TodoId>(COMPLETE_TODO)
-
-const toggleAllTodos = actionCreator(COMPLETE_ALL)
-
-const clearCompletedTodos = actionCreator(CLEAR_COMPLETED)
-
-export {
-  addTodo,
-  deleteTodo,
-  editTodo,
-  toggleTodo,
-  toggleAllTodos,
-  clearCompletedTodos
-}
+export const addTodo = actionCreator<string>(ADD_TODO)
+export const clearCompletedTodos = actionCreator(CLEAR_COMPLETED)
+export const deleteTodo = actionCreator<TodoId>(DELETE_TODO)
+export const editTodo = actionCreator<{todoId: TodoId, newText: string}>(EDIT_TODO)
+export const toggleAllTodos = actionCreator(COMPLETE_ALL)
+export const toggleTodo = actionCreator<TodoId>(COMPLETE_TODO)
