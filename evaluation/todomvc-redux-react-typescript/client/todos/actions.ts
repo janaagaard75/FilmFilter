faff15bbc627e1771001781d8698e6fc015823e2
Jan Aagaard <jan@aagaard.net>
2016-11-15 21:51:50 +0100

    Clean up

--- evaluation/todomvc-redux-react-typescript/client/todos/actions.ts
+++ evaluation/todomvc-redux-react-typescript/client/todos/actions.ts
@@ -1,11 +1,10 @@
 import actionCreatorFactory from 'redux-typescript-actions'
 import { assign } from 'lodash'
 
-import { TodoId, Todo } from './model'
+import { TodoId } from './model'
 
 const actionCreator = actionCreatorFactory()
 
-// TODO: These action creators should be named differently. E.g. createAddTodo or todoCreated.
 export const createAddTodo = actionCreator<string>('ADD_TODO')
 export const createClearCompletedTodos = actionCreator('CLEAR_COMPLETED_TODOS')
 export const createDeleteTodo = actionCreator<TodoId>('DELETE_TODO')
