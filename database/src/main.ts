e133cd95124927a2e507a6e5b46fe79c7b409566
Jan Aagaard <jan@aagaard.net>
2016-12-26 17:46:56 +0100

    Add interfaces

--- database/src/main.ts
+++ database/src/main.ts
@@ -13,19 +13,24 @@ const readScraperOutput = (filename: string): Array<string> => {
 
 interface MovieLine {
   danishTitle: string
+  movieUrl: string
   originalTitle: string
   posterUrl: string
-  movieUrl: string
 }
 
 type VersionFlag = '2D' | '3D' | 'IMAX 2D' | 'IMAX 3D' | 'dansk tale' | 'S\u00e6rvisning'
 
 interface ShowingLine {
-  start: string
-  showingUrl: string
-  theatherUrl: string
   movieUrl: string
+  showingUrl: string
+  start: string
+  theatherUrl: string
   versionFlags: Array<VersionFlag>
+}
+
+interface TheaterLine {
+  name: string
+  theaterUrl: string
 }
 
 const movieLines = readScraperOutput('movies.jsonl')
