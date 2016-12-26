da4e9056a33fdbcb2cb45737cb85d6b01f66b2db
Jan Aagaard <jan@aagaard.net>
2016-12-26 14:33:45 +0100

    Read the output files

--- database/main.ts
+++ database/main.ts
@@ -1,2 +1,11 @@
-const message = "Hello World"
-console.log(message);
+const fs = require('fs')
+
+const outputFolder = '../scraper/output/'
+
+const movies = fs.readFileSync(outputFolder + 'movies.jsonl', 'utf8').split('\n')
+const showings = fs.readFileSync(outputFolder + 'showings.jsonl', 'utf8').split('\n')
+const theaters = fs.readFileSync(outputFolder + 'theaters.jsonl', 'utf8').split('\n')
+
+console.info(`${movies.length} movies.`)
+console.info(`${showings.length} showings.`)
+console.info(`${theaters.length} theathers.`)
