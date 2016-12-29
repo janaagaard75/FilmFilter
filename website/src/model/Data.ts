05ec88b391705678148bd0d81549715b403df043
Jan Aagaard <jan@aagaard.net>
2016-12-29 14:32:52 +0100

    Add interface for imported data

---
+++ website/src/model/Data.ts
@@ -0,0 +1,27 @@
+interface Movie {
+  readonly danishTitle?: string
+  readonly movieUrl: string
+  readonly originalTitle: string
+  readonly posterUrl: string
+}
+
+interface Showing {
+  readonly dubbed: boolean
+  readonly imax: boolean
+  readonly movieId: number
+  readonly specialShowing: boolean
+  readonly start: Date
+  readonly theaterId: number
+  readonly threeD: boolean
+}
+
+interface Theater {
+  readonly name: string
+  readonly theatherUrl: string
+}
+
+export interface Data {
+  movies: Array<Movie>,
+  showings: Array<Showing>,
+  theaters: Array<Theater>
+}
