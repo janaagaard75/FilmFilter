ee1320801ad6440e08661ebdcdbedf13e18b6e55
Jan Aagaard <jan@aagaard.net>
2016-12-29 14:27:41 +0100

    Lower case variable

--- website/src/routes.ts
+++ website/src/routes.ts
@@ -1,12 +1,12 @@
 import { ConnectedApp } from "./ConnectedApp"
 import { TypedRoute } from "./model/TypedRoute"
 
-export const HomeRoute: TypedRoute<() => string> = new TypedRoute(
+export const homeRoute: TypedRoute<() => string> = new TypedRoute(
   ConnectedApp,
   "/",
   () => "/"
 )
 
 export const allRoutes = [
-  HomeRoute
+  homeRoute
 ]
