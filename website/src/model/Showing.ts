3020982c35a66b11996b17a5413d46453afe5168
Jan Aagaard <jan@aagaard.net>
2016-12-29 17:50:42 +0100

    Add Showing class

---
+++ website/src/model/Showing.ts
@@ -0,0 +1,33 @@
+import { MovieData } from './Data'
+import { ShowingData } from './Data'
+import { Store } from './Store'
+
+export class Showing {
+  constructor(data: ShowingData, store: Store) {
+    this.dubbed = data.dubbed
+    this.imax = data.imax
+    this.showingUrl = 'http://www.kino.dk/' + data.showingUrl
+    this.specialShowing = data.specialShowing
+    this.start = data.start
+    this.theaterId = data.theaterId
+    this.threeD = data.threeD
+
+    if (data.movieId === -1) {
+      this.movie = undefined
+    }
+    else {
+      this.movie = store.data.movies[data.movieId]
+    }
+  }
+
+  public readonly dubbed: boolean
+  public readonly imax: boolean
+  public readonly movie?: MovieData
+  public readonly showingUrl: string
+  public readonly specialShowing: boolean
+  public readonly start: Date
+  public readonly theaterId: number
+  public readonly threeD: boolean
+}
+
+// TODO: This class is not yet used.
