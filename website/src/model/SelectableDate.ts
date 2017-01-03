4879d0987a088f076be307d0fcd3d4755f206e18
Jan Aagaard <uxj@kmd.dk>
2017-01-03 06:40:55 +0100

    Add TODO

--- website/src/model/SelectableDate.ts
+++ website/src/model/SelectableDate.ts
@@ -10,6 +10,7 @@ export class SelectableDate {
   public readonly date: Moment
   @observable public selected: boolean
 
+  // TODO: Should this be part of the model? And if so, what about a formatting of the start in Showing.ts?
   public get label(): string {
     const label = this.date.format("dd D/M")
     return label
