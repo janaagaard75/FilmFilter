a2a34caea5578714a5a50372f6ace72e3b2cbcb3
Jan Aagaard <uxj@kmd.dk>
2017-01-04 07:06:23 +0100

    Add observer

--- website/src/components/bootstrap/Collapse.tsx
+++ website/src/components/bootstrap/Collapse.tsx
@@ -1,16 +1,14 @@
 import * as classNames from "classnames"
 import * as React from "react"
 import { Component } from "react"
+import { observer } from "mobx-react"
 
 interface Props {
   expanded: boolean
 }
 
+@observer
 export class Collapse extends Component<Props, void> {
-  constructor(props: void, context?: any) {
-    super(props, context)
-  }
-
   public render() {
     const cssClasses = classNames({
       collapse: true,
