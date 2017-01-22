02d2223113c1190b2d82ec5b99be322ad34ecb2c
Jan Aagaard <jan@aagaard.net>
2017-01-22 23:13:06 +0100

    Rebuild

--- data/dist/index.js
+++ data/dist/index.js
@@ -3,19 +3,20 @@ const express = require("express");
 const node_fetch_1 = require("node-fetch");
 const fs = require("fs");
 const app = express();
-app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
-const apiKey = 'a706cc2fdb8e4ce89f00aed30a6fc2a0';
-const host = 'storage.scrapinghub.com';
+const apiKey = "a706cc2fdb8e4ce89f00aed30a6fc2a0";
+const host = "storage.scrapinghub.com";
 const jobId = 142200;
-const outputDir = 'output';
+const outputDir = "output";
+const port = 5000;
+app.set("port", (process.env.PORT || port));
+app.use(express.static(__dirname + "/public"));
 if (!fs.existsSync(outputDir)) {
     fs.mkdirSync(outputDir);
 }
 node_fetch_1.default(`https://${apiKey}:@${host}/jobq/${jobId}/list`)
     .then(jobsResponse => jobsResponse.text())
     .then(jobList => {
-    jobList.split('\n')
+    jobList.split("\n")
         .slice(0, 3) // TODO: This is only correct if the three jobs are all done.
         .forEach(jobInfoString => {
         const jobInfo = JSON.parse(jobInfoString);
@@ -27,8 +28,3 @@ node_fetch_1.default(`https://${apiKey}:@${host}/jobq/${jobId}/list`)
         });
     });
 });
-// fetch('https://a706cc2fdb8e4ce89f00aed30a6fc2a0:@storage.scrapinghub.com/items/142200/45')
-//   .then(response => response.text())
-//   .then(body => {
-//     fs.writeFileSync('output.jsonl', body)
-//   }) 
