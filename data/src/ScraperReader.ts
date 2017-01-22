cd64321c43a1e7a944ead484b1c6f0aac6dfb7ed
Jan Aagaard <jan@aagaard.net>
2017-01-22 23:48:19 +0100

    Rename to JsonlParser

--- data/src/ScraperReader.ts
+++ data/src/ScraperReader.ts
@@ -1,22 +1,11 @@
 import * as fs from 'fs'
 
-export class ScraperReader {
-  public static readData<TLine>(filename: string): Array<TLine> {
-    const lines = this.readScraperOutput(filename)
+export class JsonlParser {
+  public static parseLines<TLine>(lines: string): Array<TLine> {
     const parsed = lines
+      .split("\n")
       .filter(line => line.length >= 1)
       .map(line => JSON.parse(line) as TLine)
     return parsed
-  }
-
-  private static readJsonLines(path: string): Array<string> {
-    const content = fs.readFileSync(path, 'utf8')
-    const lines = content.split('\n')
-    return lines
-  }
-
-  private static readScraperOutput(filename: string): Array<string> {
-    const lines = this.readJsonLines('../scraper/output/' + filename)
-    return lines
   }
 }
