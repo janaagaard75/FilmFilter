bdfc7408a2fc3e6dc5a1b4098c46fa841e029a59
Jan Aagaard <jan@aagaard.net>
2017-01-27 17:54:58 +0100

    Use double quotes

--- data/src/UrlUtil.ts
+++ data/src/UrlUtil.ts
@@ -1,6 +1,6 @@
 export class UrlUtil {
   public static removeStandardPrefix(url: string): string {
-    const standardUrlPrefix = 'http://www.kino.dk/'
+    const standardUrlPrefix = "http://www.kino.dk/"
     const slicedUrl = url.slice(standardUrlPrefix.length)
     return slicedUrl
   }
