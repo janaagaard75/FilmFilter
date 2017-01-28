02335ae70b6042d83f20d230fbaee39665527198
Jan Aagaard <jan@aagaard.net>
2017-01-28 22:55:08 +0100

    Store data in local storage

--- website/src/model/DataGetter.ts
+++ website/src/model/DataGetter.ts
@@ -1,14 +1,41 @@
 import { Data } from "./data/Data"
 import { DataFetcher } from "./DataFetcher"
+import { DataStorer } from "./DataStorer"
 
 export class DataGetter {
   public static async getData(): Promise<Data> {
-    const data = await DataFetcher.fetchData()
+    const storedData = DataStorer.loadData()
 
-    if (data === undefined) {
+    if (storedData === undefined || DataGetter.isOutdated(storedData.timestamp)) {
+      const fetchedData = await DataGetter.fetchAndUpdateStoredData()
+      return fetchedData
+    }
+
+    return storedData.data
+  }
+
+  public static async fetchAndUpdateStoredData(): Promise<Data> {
+    const fetchedData = await DataFetcher.fetchData()
+
+    if (fetchedData === undefined) {
       throw new Error("Could not fetch data.")
     }
 
-    return data
+    DataStorer.saveData(fetchedData)
+    return fetchedData
+  }
+
+  private static isOutdated(timestamp: Date) {
+    const now = new Date()
+    // tslint:disable-next-line prefer-const
+    let latestDataFetch = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 5, 0, 0)
+    if (latestDataFetch.valueOf() > now.valueOf()) {
+      latestDataFetch.setDate(latestDataFetch.getDate() - 1)
+    }
+
+    const millisecondsIn24Hours = 24 * 60 * 60 * 1000
+    const millisecondsSinceLatestFetch = latestDataFetch.valueOf() - timestamp.valueOf()
+    const isOutdated = millisecondsSinceLatestFetch > millisecondsIn24Hours
+    return isOutdated
   }
 }
--- website/src/model/DataStorer.ts
+++ website/src/model/DataStorer.ts
@@ -1,21 +1,31 @@
 import { Data } from "./data/Data"
 
+interface TimestampedData {
+  data: Data,
+  timestamp: Date
+}
+
 // TODO: Add a date to the data being stored.
 export class DataStorer {
   private static readonly dataKey = "data"
 
-  public static loadData(): Data | undefined {
+  public static loadData(): TimestampedData | undefined {
     const dataString = localStorage.getItem(this.dataKey)
 
     if (dataString === null) {
       return undefined
     }
 
-    return JSON.parse(dataString)
+    return JSON.parse(dataString) as TimestampedData
   }
 
   public static saveData(data: Data) {
-    const dataString = JSON.stringify(data)
+    const storedData: TimestampedData = {
+      data: data,
+      timestamp: new Date()
+    }
+
+    const dataString = JSON.stringify(storedData)
     localStorage.setItem(this.dataKey, dataString)
   }
 }
