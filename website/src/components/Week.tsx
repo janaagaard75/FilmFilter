d94de342c0489a24bfef53c470b7159976e69274
Jan Aagaard <jan@aagaard.net>
2017-02-05 14:45:28 +0100

    Use models's toggleSelection

--- website/src/components/Week.tsx
+++ website/src/components/Week.tsx
@@ -6,7 +6,6 @@ import { SelectableDate } from "../model/SelectableDate"
 
 interface Props {
   dates: Array<SelectableDate>
-  toggleDateSelection: () => void
 }
 
 export class Week extends Component<Props, void> {
@@ -14,7 +13,7 @@ export class Week extends Component<Props, void> {
     return (
       <tr>
         {this.props.dates.map(date =>
-          <DateItem date={date} toggleDateSelection={this.props.toggleDateSelection} />
+          <DateItem date={date}/>
         )}
       </tr>
     )
