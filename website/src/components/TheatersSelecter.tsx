ac43e4c25f6d0910fcb7a26d45bc5d90bad65078
Jan Aagaard <jan@aagaard.net>
2017-02-13 21:29:08 +0100

    Show favorite theaters above the others

--- website/src/components/TheatersSelecter.tsx
+++ website/src/components/TheatersSelecter.tsx
@@ -19,10 +19,27 @@ export class TheatersSelecter extends Component<Props, void> {
       ? "Vælg biograf"
       : "Biograf: " + this.props.selectedTheaters.map(theater => theater.name).join(", ")
 
+    const favoritedTheaters = this.props.theaters.filter(theater => theater.favorited)
+    const notFavoritedTheaters = this.props.theaters.filter(theater => !theater.favorited)
+    const bothTypes = favoritedTheaters.length >= 1 && notFavoritedTheaters.length >= 1
+
     return (
-    <CollapsibleCard header={header}>
+      <CollapsibleCard header={header}>
         <div className="row">
-          {this.props.theaters.map(theater =>
+          {favoritedTheaters.map(theater =>
+            <TheaterItem
+              key={theater.theatherUrl}
+              theater={theater}
+            />
+          )}
+        </div>
+        {bothTypes ? (
+          <hr/>
+        ) : (
+           ""
+        )}
+        <div className="row">
+          {notFavoritedTheaters.map(theater =>
             <TheaterItem
               key={theater.theatherUrl}
               theater={theater}
