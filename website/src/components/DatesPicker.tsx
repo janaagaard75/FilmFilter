f1f277ff928272d9aac3a729d534ee5fd9b03a11
Jan Aagaard <jan@aagaard.net>
2017-03-06 21:05:25 +0100

    Remove responsiveness from tables

--- website/src/components/DatesPicker.tsx
+++ website/src/components/DatesPicker.tsx
@@ -13,7 +13,7 @@ interface Props {
 export class DatesPicker extends Component<Props, void> {
   public render() {
     return (
-      <table className="table table-sm table-responsive">
+      <table className="table table-sm">
         <tbody>
           <tr>
             <td>Ma</td>
