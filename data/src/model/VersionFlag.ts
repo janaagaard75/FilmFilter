5ba3803c8a0e54331373793f3db19a0ebdf03301
Jan Aagaard <uxj@kmd.dk>
2017-03-08 07:46:50 +0100

    Add Babybio version

--- data/src/model/VersionFlag.ts
+++ data/src/model/VersionFlag.ts
@@ -1,1 +1,1 @@
-export type VersionFlag = "2D" | "3D" | "IMAX 2D" | "IMAX 3D" | "dansk tale" | "Særvisning"
+export type VersionFlag = "2D" | "3D" | "IMAX 2D" | "IMAX 3D" | "dansk tale" | "Særvisning" | "Babybio"
