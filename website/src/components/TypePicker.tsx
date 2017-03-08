a8d986b75c3544e839f7f73a0105b33b9e20a853
Jan Aagaard <uxj@kmd.dk>
2017-03-08 08:19:25 +0100

    Add Checkbox component

--- website/src/components/TypePicker.tsx
+++ website/src/components/TypePicker.tsx
@@ -2,6 +2,7 @@ import * as React from "react"
 import { Component } from "react"
 import { observer } from "mobx-react"
 
+import { Checkbox } from "./bootstrap/Checkbox"
 import { Dimension } from "../model/Dimension"
 import { Store } from "../model/Store"
 
@@ -15,39 +16,24 @@ export class TypePicker extends Component<Props, void> {
     return (
       <div className="row">
         <div className="col-sm-6 col-md-3 mb-4">
-          <div className="form-check">
-            <label className="form-check-label">
-              <input
-                type="radio"
-                className="form-check-input"
-                onClick={() => { this.props.store.selectedDimension = Dimension.TwoD }}
-                checked={this.props.store.selectedDimension === Dimension.TwoD}
-              />
-              {" "}2D
-            </label>
-          </div>
-          <div className="form-check">
-            <label className="form-check-label">
-              <input
-                type="radio"
-                className="form-check-input"
-                onClick={() => { this.props.store.selectedDimension = Dimension.ThreeD }}
-                checked={this.props.store.selectedDimension === Dimension.ThreeD}
-              />
-              {" "}3D
-            </label>
-          </div>
-          <div className="form-check">
-            <label className="form-check-label">
-              <input
-                type="radio"
-                className="form-check-input"
-                onClick={() => { this.props.store.selectedDimension = Dimension.Both }}
-                checked={this.props.store.selectedDimension === Dimension.Both}
-              />
-              {" "}Ligegyldigt
-            </label>
-          </div>
+          <Checkbox
+            checked={this.props.store.selectedDimension === Dimension.Both}
+            onClick={() => { this.props.store.selectedDimension = Dimension.Both }}
+          >
+            2D eller 3D
+          </Checkbox>
+          <Checkbox
+            checked={this.props.store.selectedDimension === Dimension.TwoD}
+            onClick={() => { this.props.store.selectedDimension = Dimension.TwoD }}
+          >
+            2D
+          </Checkbox>
+          <Checkbox
+            checked={this.props.store.selectedDimension === Dimension.ThreeD}
+            onClick={() => { this.props.store.selectedDimension = Dimension.ThreeD }}
+          >
+            3D
+          </Checkbox>
         </div>
         <div className="col-sm-6 col-md-3 mb-4">
           <div className="form-check">
---
+++ website/src/components/bootstrap/Checkbox.tsx
@@ -0,0 +1,27 @@
+import * as React from "react"
+import { Component } from "react"
+import { observer } from "mobx-react"
+
+interface Props {
+  checked: boolean
+  onClick: () => void
+}
+
+@observer
+export class Checkbox extends Component<Props, void> {
+  public render() {
+    return (
+      <div className="form-check">
+        <label className="form-check-label">
+          <input
+            type="radio"
+            checked={this.props.checked}
+            className="form-check-input"
+            onClick={this.props.onClick}
+          />
+          {" "}{this.props.children}
+        </label>
+      </div>
+    )
+  }
+}
