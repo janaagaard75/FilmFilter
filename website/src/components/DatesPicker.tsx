586b206a3c72ffff6e9b94ee55582c9aaf2d0b92
Jan Aagaard <jan@aagaard.net>
2017-03-15 21:53:14 +0100

    Add time interval controls

--- website/src/components/DatesPicker.tsx
+++ website/src/components/DatesPicker.tsx
@@ -1,5 +1,6 @@
 import * as React from "react"
 import { Component } from "react"
+import { KeyboardEvent } from "react"
 import { observer } from "mobx-react"
 
 import { SelectableDate } from "../model/SelectableDate"
@@ -9,28 +10,80 @@ interface Props {
   weeks: Array<Array<SelectableDate>>
 }
 
+interface State {
+  startIntervalFrom: string
+  startIntervalTo: string
+}
+
 @observer
-export class DatesPicker extends Component<Props, void> {
+export class DatesPicker extends Component<Props, State> {
+  constructor(props?: Props, context?: any) {
+    super(props, context)
+
+    this.state = {
+      startIntervalFrom: "",
+      startIntervalTo: ""
+    }
+  }
+
+  private handleKeyUpFrom(formEvent: KeyboardEvent<HTMLInputElement>) {
+    if (formEvent.key === "Escape") {
+      formEvent.currentTarget.value = ""
+    }
+
+    this.setState({
+      startIntervalFrom: formEvent.currentTarget.value
+    })
+  }
+
+  private handleKeyUpTo(formEvent: KeyboardEvent<HTMLInputElement>) {
+    if (formEvent.key === "Escape") {
+      formEvent.currentTarget.value = ""
+    }
+
+    this.setState({
+      startIntervalTo: formEvent.currentTarget.value
+    })
+  }
+
   public render() {
     return (
-      <table className="table table-sm table-text-center">
-        <thead>
-          <tr>
-            <th>Ma</th>
-            <th>Ti</th>
-            <th>On</th>
-            <th>To</th>
-            <th>Fr</th>
-            <th>Lø</th>
-            <th>Sø</th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.props.weeks.map(week =>
-            <Week key={"week-" + week[0].key} dates={week}/>
-          )}
-        </tbody>
-      </table>
+      <div>
+        <div className="form-inline mb-3">
+          <label className="mr-3">Fra:</label>
+          <input
+            type="time"
+            className="form-control col-2"
+            placeholder="Søg efter film"
+            onKeyUp={e => this.handleKeyUpFrom(e)}
+          />
+          <label className="ml-5 mr-3">Til:</label>
+          <input
+            type="time"
+            className="form-control col-2"
+            placeholder="Søg efter film"
+            onKeyUp={e => this.handleKeyUpTo(e)}
+          />
+        </div>
+        <table className="table table-sm table-text-center">
+          <thead>
+            <tr>
+              <th>Ma</th>
+              <th>Ti</th>
+              <th>On</th>
+              <th>To</th>
+              <th>Fr</th>
+              <th>Lø</th>
+              <th>Sø</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.weeks.map(week =>
+              <Week key={"week-" + week[0].key} dates={week}/>
+            )}
+          </tbody>
+        </table>
+      </div>
     )
   }
 }
