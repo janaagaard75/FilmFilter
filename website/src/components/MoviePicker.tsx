5c266dc551cca127734b9e016dca90d963c8d087
Jan Aagaard <jan@aagaard.net>
2017-03-15 20:56:54 +0100

    Center movie names

--- website/src/components/MoviePicker.tsx
+++ website/src/components/MoviePicker.tsx
@@ -15,7 +15,7 @@ export class MoviePicker extends Component<Props, void> {
   public render() {
     const cssClasses = classNames(
       "col-6 col-sm-4 col-md-3 col-lg-1-of-5 col-xl-2",
-      "clickable",
+      "clickable text-center mb-3",
       this.props.cssClass,
       {
         "selected-item": this.props.movie.selected
