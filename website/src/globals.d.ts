a3b2ac7826451bf7a3bd1d53c586dd5669bf1622
Jan Aagaard <jan@aagaard.net>
2017-03-15 20:14:10 +0100

    Remove unused require

--- website/src/globals.d.ts
+++ website/src/globals.d.ts
@@ -1,7 +1,1 @@
 declare var __BUILD_TIMESTAMP__: number
-
-declare var require: {
-  <T>(path: string): T
-  (paths: Array<string>, callback: (...modules: Array<any>) => void): void
-  ensure: (paths: Array<string>, callback: (require: <T>(path: string) => T) => void) => void
-}
