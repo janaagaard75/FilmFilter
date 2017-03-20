9ed4284c7bad313d1761ce4783af968672abe190
Jan Aagaard <jan@aagaard.net>
2017-03-20 06:21:27 +0100

    Introduce FiltersJson interface

--- website/src/model/filters/Filters.ts
+++ website/src/model/filters/Filters.ts
@@ -6,9 +6,22 @@ import { TimeInterval } from "../TimeInterval"
 
 // TODO: Filters has to be an interface since it's being serialized to and from JSON.
 export class Filters {
+  constructor(filtersJson: FiltersJson) {
+    this.dimensions.twoD = filtersJson.dimensions.twoD
+    ...
+  }
+
   public readonly dimensions = new Dimensions()
   public readonly filmType = new FilmType()
   public readonly language = new Language()
   public readonly showingType = new ShowingType()
   public readonly startInterval = new TimeInterval()
+}
+
+export interface FiltetsJson {
+  dimensions: DimensionsJson
+  filmType: FilmTypeJson
+  language: LanguageJson
+  showingType: ShowingTypeJson
+  startInterval: StartIntervalJson
 }
