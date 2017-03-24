283e56e330247ccf43a25f982b6da37e88f8f3aa
Jan Aagaard <uxj@kmd.dk>
2017-03-24 07:10:46 +0100

    Add line break

--- website/src/model/RouteComponent.ts
+++ website/src/model/RouteComponent.ts
@@ -3,4 +3,5 @@ import { ComponentClass } from "react"
 import { RouteComponentProps } from "react-router"
 
 export class RouteComponent<Params, RouteParams, State> extends Component<RouteComponentProps<Params, RouteParams>, State> { }
+
 export type RouteComponentClass<Params, RouteParams> = ComponentClass<RouteComponentProps<Params, RouteParams>>
