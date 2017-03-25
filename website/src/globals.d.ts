f8219ac74b5441e4ac729a2d0a605f644c70a222
Jan Aagaard <jan@aagaard.net>
2017-03-25 16:14:18 +0100

    Strongly type process

--- website/src/globals.d.ts
+++ website/src/globals.d.ts
@@ -1,2 +1,8 @@
+interface Process {
+  env: {
+    NODE_ENV: "development" | "production"
+  }
+}
+
 declare const __BUILD_TIMESTAMP__: number
-declare const process: any
+declare const process: Process
