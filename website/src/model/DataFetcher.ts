65edf363da547d85f8fff16b90403c2f4d886834
Jan Aagaard <jan@aagaard.net>
2017-03-27 22:43:37 +0200

    Download compressed data

--- website/src/model/DataFetcher.ts
+++ website/src/model/DataFetcher.ts
@@ -1,14 +1,16 @@
 import "whatwg-fetch"
+import * as LZString from "lz-string"
 
 import { Data } from "./data/Data"
 
 export class DataFetcher {
-  private static readonly dataUrl = "https://film-filter-data.herokuapp.com/"
+  private static readonly dataUrl = "https://film-filter-data.herokuapp.com/compressed"
 
   public static async fetchData(): Promise<Data | undefined> {
     try {
       const response = await window.fetch(this.dataUrl, { mode: "cors" })
-      const dataString = await response.text()
+      const compressedString = await response.text()
+      const dataString = LZString.decompressFromBase64(compressedString)
       const data = JSON.parse(dataString) as Data
       return data
     }
