3aefa4ce4d40209a5e4ac7685e38e7fe7c0cd0dc
Jan Aagaard <uxj@kmd.dk>
2017-04-03 07:34:36 +0200

    Logging state changes in CurrentState

--- website/src/components/CurrentState.tsx
+++ website/src/components/CurrentState.tsx
@@ -1,8 +1,10 @@
 import * as React from "react"
 import { Component } from "react"
 import { observer } from "mobx-react"
+import { reaction } from "mobx"
 
 import { AppState } from "../model/AppState"
+import { Logger } from "../utilities/Logger"
 import { Store } from "../model/Store"
 
 interface Props {
@@ -11,6 +13,19 @@ interface Props {
 
 @observer
 export class CurrentState extends Component<Props, void> {
+  constructor(props: Props, context?: any) {
+    super(props, context)
+
+    reaction(
+      () => this.props.store.state,
+      () => this.forceUpdate()
+    )
+  }
+
+  // private forceRender() {
+  //   Logger.log(`Forcing update render of CurrentState. State: ${this.props.store.state}, ${this.props.store.stateDescription}.`)
+  // }
+
   public render() {
     return (
       <span>
