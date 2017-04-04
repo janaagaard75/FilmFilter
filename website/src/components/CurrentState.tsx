1f0338d16580633e24aee3a1c9531064a513b582
Jan Aagaard <jan@aagaard.net>
2017-04-04 04:37:20 +0200

    Enable no-unused-variable for `yarn lint`

--- website/src/components/CurrentState.tsx
+++ website/src/components/CurrentState.tsx
@@ -1,13 +1,8 @@
 import * as React from "react"
-import { autorun } from "mobx"
 import { Component } from "react"
 import { observer } from "mobx-react"
-import { reaction } from "mobx"
-import { whyRun } from "mobx"
-import { isObservable } from "mobx"
 
 import { AppState } from "../model/AppState"
-import { Logger } from "../utilities/Logger"
 import { Store } from "../model/Store"
 
 interface Props {
