4040d0847b01f97da1011b1396f2b3938ab81948
Jan Aagaard <jan@aagaard.net>
2017-04-10 21:12:34 +0200

    More classy implementation of Counter

--- website/src/workers/Counter.ts
+++ website/src/workers/Counter.ts
@@ -1,12 +1,20 @@
-self.addEventListener("message", (e: MessageEvent) => {
-  const max = 1e8
-  for (let i = 1; i <= 5; i++) {
-    for (let j = 1; j <= max; j++) {
-      // Comment required to satify TSlint.
-    }
-
-    (self as DedicatedWorkerGlobalScope).postMessage(`Hello from worker, ${i}.`)
+class Counter {
+  public messageReceived(e: MessageEvent) {
+    (self as DedicatedWorkerGlobalScope).postMessage(e.data)
+    this.count()
   }
 
-  (self as DedicatedWorkerGlobalScope).postMessage(e.data);
-}, false);
+  private count(): void {
+    const max = 1e8
+    for (let i = 1; i <= 5; i++) {
+      for (let j = 1; j <= max; j++) {
+        // Comment here to satify TSlint.
+      }
+
+      (self as DedicatedWorkerGlobalScope).postMessage(`Hello from worker, ${i}.`)
+    }
+  }
+}
+
+const counter = new Counter()
+self.addEventListener("message", (e: MessageEvent) => counter.messageReceived(e), false)
