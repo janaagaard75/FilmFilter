659762c9b0ddeb089332a2e4cce3d86a43460988
Jan Aagaard <uxj@kmd.dk>
2017-04-10 14:09:45 +0200

    Worker in TypeScript

--- website/src/components/Counter.worker.ts
+++ website/src/components/Counter.worker.ts
@@ -2,10 +2,11 @@ self.addEventListener("message", e => {
   const max = 1e8
   for (let i = 1; i <= 5; i++) {
     for (let j = 1; j <= max; j++) {
+      // Comment required to satify TSlint.
     }
 
-    self.postMessage(`Hello from worker, ${i}.`)
+    (self.postMessage as any)(`Hello from worker, ${i}.`)
   }
 
-  self.postMessage(e.data);
+  (self.postMessage as any)(e.data);
 }, false);
