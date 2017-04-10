71b43f1728f761218e0932cd194b4524cb314934
Jan Aagaard <jan@aagaard.net>
2017-04-10 19:35:53 +0200

    Add comment

--- website/src/globals.d.ts
+++ website/src/globals.d.ts
@@ -7,6 +7,7 @@ interface Process {
 declare const __BUILD_TIMESTAMP__: number
 declare const process: Process
 
+// See https://github.com/TypeStrong/ts-loader#loading-other-resources-and-code-splitting.
 declare const require: {
     <T>(path: string): T
     (paths: Array<string>, callback: (...modules: Array<any>) => void): void
