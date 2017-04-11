865d6ec569fc719a5917d4e2248d67338339d865
Jan Aagaard <uxj@kmd.dk>
2017-04-11 07:37:28 +0200

    Reverse if to unindent a level

--- website/src/main/utilities/Logger.ts
+++ website/src/main/utilities/Logger.ts
@@ -3,18 +3,20 @@ import { Environment } from "./Environment"
 export class Logger {
   private static lastTimestamp: number | undefined
 
-  public static log(message: string) {
-    if (Environment.inDevelopmentMode()) {
-      const thisTimestamp = Date.now()
-      if (this.lastTimestamp !== undefined) {
-        const executionTime = thisTimestamp - this.lastTimestamp
-        const formatted = executionTime.toLocaleString("da-DK")
-        message += ` (${formatted} ms)`
-      }
-
-      // tslint:disable-next-line:no-console
-      console.info(message)
-      this.lastTimestamp = thisTimestamp
+  public static log(message: string): void {
+    if (!Environment.inDevelopmentMode()) {
+      return
     }
+
+    const thisTimestamp = Date.now()
+    if (this.lastTimestamp !== undefined) {
+      const executionTime = thisTimestamp - this.lastTimestamp
+      const formatted = executionTime.toLocaleString("da-DK")
+      message += ` (${formatted} ms)`
+    }
+
+    // tslint:disable-next-line:no-console
+    console.info(message)
+    this.lastTimestamp = thisTimestamp
   }
 }
