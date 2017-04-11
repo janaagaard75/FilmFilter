b77c58807c8d70a74b20f3a8b7ed8981af15913b
Jan Aagaard <uxj@kmd.dk>
2017-04-11 14:18:51 +0200

    Add TODO

--- website/src/main/model/data/Data.ts
+++ website/src/main/model/data/Data.ts
@@ -2,6 +2,7 @@ import { MovieData } from "./MovieData"
 import { ShowingData } from "./ShowingData"
 import { TheaterData } from "./TheaterData"
 
+// TODO: Figure out a better name for this. RawData? ApiData? Same goes for the associated *Data classes.
 export interface Data {
   movies: Array<MovieData>
   showings: Array<ShowingData>
