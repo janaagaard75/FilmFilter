751783770a3addbaabc8f17d331a516969710d34
Jan Aagaard <jan@aagaard.net>
2017-04-13 11:13:11 +0200

    Remove temporary worker

--- website/src/main/components/CurrentState.tsx
+++ website/src/main/components/CurrentState.tsx
@@ -13,35 +13,13 @@ interface Props {
 
 @observer
 export class CurrentState extends Component<Props, void> {
-  @observable private progress: number = 0
-
-  private count(): void {
-    const CounterWorker = require("../../workers/Counter") as any
-    const counterWorker = new CounterWorker() as Worker
-    counterWorker.addEventListener("message", (e: any) => {
-      // tslint:disable-next-line:no-console
-      console.info("Message from worker:", e)
-    })
-    counterWorker.postMessage({ a: 1 })
-  }
-
-  @computed
-  private get progressPercent(): string {
-    const progressInPercent = Math.floor(100 * this.progress)
-    return `${progressInPercent}%`
-  }
-
   public render() {
     return (
       <span>
-        <button className="btn btn-primary btn-sm mr-3" onClick={() => this.count()}>Count</button>
-        <span className="mr-5">{this.progressPercent}</span>
-        <span>
-          {this.props.store.appState !== AppState.Idle
-            ? <span className="form-control-static mr-3">{this.props.store.stateDescription} <i className="fa fa-spinner fa-pulse"/></span>
-            : undefined
-          }
-        </span>
+        {this.props.store.appState !== AppState.Idle
+          ? <span className="form-control-static mr-3">{this.props.store.stateDescription} <i className="fa fa-spinner fa-pulse"/></span>
+          : undefined
+        }
       </span>
     )
   }
