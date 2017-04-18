d38b5a8552b09363e82bc84dc0294e45fd91bbca
Jan Aagaard <jan@aagaard.net>
2017-04-18 19:23:14 +0200

    Add separate methods to control the timer

--- website/src/main/utilities/Logger.ts
+++ website/src/main/utilities/Logger.ts
@@ -8,15 +8,34 @@ export class Logger {
       return
     }
 
-    const thisTimestamp = Date.now()
-    if (this.lastTimestamp !== undefined) {
-      const executionTime = thisTimestamp - this.lastTimestamp
-      const formatted = executionTime.toLocaleString("da-DK")
-      message += ` (${formatted} ms)`
-    }
-
     // tslint:disable-next-line:no-console
     console.info(message)
-    this.lastTimestamp = thisTimestamp
+  }
+
+  public static logAndTime(message: string): void {
+    if (!Environment.inDevelopmentMode()) {
+      return
+    }
+
+    if (this.lastTimestamp === undefined) {
+      throw new Error("The timer hasn't been started. Call startTimer() before calling logAndTime().")
+    }
+
+    const thisTimestamp = Date.now()
+
+    const executionTime = thisTimestamp - this.lastTimestamp
+    const formatted = executionTime.toLocaleString("da-DK")
+    const timestampedMessage = `${message} (${formatted} ms)`
+
+    // tslint:disable-next-line:no-console
+    console.info(timestampedMessage)
+  }
+
+  public static startTimer(): void {
+    if (!Environment.inDevelopmentMode()) {
+      return
+    }
+
+    this.lastTimestamp = Date.now()
   }
 }
