f4e39124d210ed1eb86323ccb56d25bf87ab13df
Jan Aagaard <jan@aagaard.net>
2017-04-19 22:33:06 +0200

    Add TODOs

--- website/src/main/model/serializable-data/SerializableData.ts
+++ website/src/main/model/serializable-data/SerializableData.ts
@@ -3,6 +3,8 @@ import { SerializableMovie } from "./SerializableMovie"
 import { SerializableShowing } from "./SerializableShowing"
 import { SerializableTheater } from "./SerializableTheater"
 
+// TODO: Prefix these interfaces with Api instead of Serializable once the old Api classes have been removed from the code.
+// TODO: Share these interfaces with the Data project.
 export interface SerializableData {
   readonly movies: Array<SerializableMovie>
   readonly showings: Array<SerializableShowing>
